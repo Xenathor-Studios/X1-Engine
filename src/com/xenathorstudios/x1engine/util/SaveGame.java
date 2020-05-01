91ec1ce6aef7ec1939623ee34dfaf6a13752bdc8
M-Dragon4 <max4battles@gmail.com>
2020-04-30 21:06:54 -0400

    Accounted for File Handler object

--- src/com/xenathorstudios/x1engine/util/SaveGame.java
+++ src/com/xenathorstudios/x1engine/util/SaveGame.java
@@ -19,14 +19,14 @@ public class SaveGame {
         /*
         Here's an example of saving data and writing comments to a save file:
 
-        comment(filepath, "This is a comment"); //Writes a comment to the save file
-        write(filepath, some_global_data_variable); //Writes data entry #1
-        comment(filepath, "This is another comment"); //Writes another comment to the save file
-        write(filepath, some_other_global_data_variable); //Writes data entry #2
+        fileHandler.comment(filepath, "This is a comment"); //Writes a comment to the save file
+        fileHandler.write(filepath, some_global_data_variable); //Writes data entry #1
+        fileHandler.comment(filepath, "This is another comment"); //Writes another comment to the save file
+        fileHandler.write(filepath, some_other_global_data_variable); //Writes data entry #2
 
         Let's say we need to save an array of items the player possesses.
         for(int i = 0; i < item_count; i++) {
-            write(filepath, items[i]);
+            fileHandler.write(filepath, items[i]);
         }
         And so on until everything that needs to be saved is saved
         */
@@ -40,8 +40,8 @@ public class SaveGame {
         try {
             /*
             The format for loading should be as follows:
-            global_data_1 = getNextDataLine(filepath);
-            global_data_2 = getNextDataLine(filepath);
+            global_data_1 = fileHandler.getNextDataLine(filepath);
+            global_data_2 = fileHandler.getNextDataLine(filepath);
             And so on until all the data is initialized
              */
             int data = Integer.parseInt(fileHandler.getNextDataLine(filepath)); //Placeholder so that the try/catch block can exist peacefully. DON'T FORGET TO REMOVE THIS LINE!
