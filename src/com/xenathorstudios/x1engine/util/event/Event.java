82be32a32693f5a33fb159b1ddc96e6c9ba82110
M-Dragon4 <max4battles@gmail.com>
2020-05-01 15:13:10 -0400

    Initial commit to event branch

---
+++ src/com/xenathorstudios/x1engine/util/event/Event.java
@@ -0,0 +1,10 @@
+package com.xenathorstudios.x1engine.util.event;
+
+/**
+ * Project: X1 Engine
+ * @author Maxwell "M_Dragon" Battles
+ * Generic Event class - there will most likely be system-specific Events that will extend from this class
+ */
+public abstract class Event {
+
+}
---
+++ src/com/xenathorstudios/x1engine/util/event/EventHandler.java
@@ -0,0 +1,10 @@
+package com.xenathorstudios.x1engine.util.event;
+
+/**
+ * Project: X1 Engine
+ * @author Maxwell "M_Dragon" Battles
+ * Generic Event Handler class - there will most likely be system-specific Event Handlers that will extend from this class
+ */
+public abstract class EventHandler {
+
+}
